1d1e0918a7ea8c2bd2bc8432d45de6f8a1f13f47
nfebe <fenn25.fn@gmail.com>
2025-01-19 00:07:59 +0100

    refactor: Modularize setup-public-share.ts
    Signed-off-by: nfebe <fenn25.fn@gmail.com>

--- cypress/e2e/files_sharing/public-share/setup-public-share.ts
+++ cypress/e2e/files_sharing/public-share/setup-public-share.ts
@@ -5,110 +5,120 @@
 import type { User } from '@nextcloud/cypress'
 import { openSharingPanel } from '../FilesSharingUtils.ts'
 
-let user: User
-let url: string
+export interface ShareContext {
+	user: User
+	url?: string
+}
+
+const defaultShareContext: ShareContext = {
+	user: {} as User,
+	url: undefined,
+}
 
 /**
  * URL of the share
+ * @param context The current share context
  */
-export function getShareUrl() {
-	if (url === undefined) {
+export function getShareUrl(context: ShareContext): string {
+	if (!context.url) {
 		throw new Error('You need to setup the share first!')
 	}
-	return url
+	return context.url
 }
 
 /**
  * Setup the available data
+ * @param context The current share context
  * @param shareName The name of the shared folder
  */
-function setupData(shareName: string) {
-	cy.mkdir(user, `/${shareName}`)
-	cy.mkdir(user, `/${shareName}/subfolder`)
-	cy.uploadContent(user, new Blob(['<content>foo</content>']), 'text/plain', `/${shareName}/foo.txt`)
-	cy.uploadContent(user, new Blob(['<content>bar</content>']), 'text/plain', `/${shareName}/subfolder/bar.txt`)
+export function setupData(context: ShareContext, shareName: string): void {
+	cy.mkdir(context.user, `/${shareName}`)
+	cy.mkdir(context.user, `/${shareName}/subfolder`)
+	cy.uploadContent(context.user, new Blob(['<content>foo</content>']), 'text/plain', `/${shareName}/foo.txt`)
+	cy.uploadContent(context.user, new Blob(['<content>bar</content>']), 'text/plain', `/${shareName}/subfolder/bar.txt`)
 }
 
 /**
  * Create a public link share
+ * @param context The current share context
  * @param shareName The name of the shared folder
  */
-function createShare(shareName: string) {
-	cy.login(user)
-	// open the files app
-	cy.visit('/apps/files')
-	// open the sidebar
-	openSharingPanel(shareName)
-	// create the share
-	cy.intercept('POST', '**/ocs/v2.php/apps/files_sharing/api/v1/shares').as('createShare')
-	cy.findByRole('button', { name: 'Create a new share link' })
-		.click()
+export function createShare(context: ShareContext, shareName: string) {
+	cy.login(context.user)
+	cy.visit('/apps/files') // Open the files app
+	openSharingPanel(shareName) // Open the sharing sidebar
 
-	// extract the link
+	cy.intercept('POST', '**/ocs/v2.php/apps/files_sharing/api/v1/shares').as('createShare')
+	cy.findByRole('button', { name: 'Create a new share link' }).click()
+
+	// Extract the share link
 	return cy.wait('@createShare')
 		.should(({ response }) => {
-			const { ocs } = response!.body
-			url = ocs?.data.url
-			expect(url).to.match(/^http:\/\//)
+			expect(response?.statusCode).to.eq(200)
+			const url = response?.body?.ocs?.data?.url
+			expect(url).to.match(/^https?:\/\//)
+			context.url = url
 		})
-		.then(() => cy.wrap(url))
+		.then(() => cy.wrap(context.url))
 }
 
 /**
  * Adjust share permissions to be editable
  */
-function adjustSharePermission() {
-	// Update the share to be a file drop
+function adjustSharePermission(): void {
 	cy.findByRole('list', { name: 'Link shares' })
 		.findAllByRole('listitem')
 		.first()
 		.findByRole('button', { name: /Actions/i })
 		.click()
-	cy.findByRole('menuitem', { name: /Customize link/i })
-		.should('be.visible')
-		.click()
+	cy.findByRole('menuitem', { name: /Customize link/i }).click()
 
-	// Enable upload-edit
-	cy.get('[data-cy-files-sharing-share-permissions-bundle]')
-		.should('be.visible')
-	cy.get('[data-cy-files-sharing-share-permissions-bundle="upload-edit"]')
-		.click()
-	// save changes
+	cy.get('[data-cy-files-sharing-share-permissions-bundle]').should('be.visible')
+	cy.get('[data-cy-files-sharing-share-permissions-bundle="upload-edit"]').click()
+
 	cy.intercept('PUT', '**/ocs/v2.php/apps/files_sharing/api/v1/shares/*').as('updateShare')
-	cy.findByRole('button', { name: 'Update share' })
-		.click()
-	cy.wait('@updateShare')
+	cy.findByRole('button', { name: 'Update share' }).click()
+	cy.wait('@updateShare').its('response.statusCode').should('eq', 200)
 }
 
 /**
  * Setup a public share and backup the state.
  * If the setup was already done in another run, the state will be restored.
  *
+ * @param shareName The name of the shared folder
  * @return The URL of the share
  */
-export function setupPublicShare(): Cypress.Chainable<string> {
-	const shareName = 'shared'
+export function setupPublicShare(shareName = 'shared'): Cypress.Chainable<string> {
+	const context: ShareContext = { ...defaultShareContext }
 
-	return cy.task('getVariable', { key: 'public-share-data' })
-		.then((data) => {
-			// eslint-disable-next-line @typescript-eslint/no-explicit-any
-			const { dataSnapshot, shareUrl } = data as any || {}
-			if (dataSnapshot) {
-				cy.restoreState(dataSnapshot)
-				url = shareUrl
-				return cy.wrap(shareUrl as string)
-			} else {
-				const shareData: Record<string, unknown> = {}
-				return cy.createRandomUser()
-					.then(($user) => { user = $user })
-					.then(() => setupData(shareName))
-					.then(() => createShare(shareName))
-					.then((value) => { shareData.shareUrl = value })
-					.then(() => adjustSharePermission())
-					.then(() => cy.saveState().then((value) => { shareData.dataSnapshot = value }))
-					.then(() => cy.task('setVariable', { key: 'public-share-data', value: shareData }))
-					.then(() => cy.log(`Public share setup, URL: ${shareData.shareUrl}`))
-					.then(() => cy.wrap(url))
-			}
-		})
+	return cy.task('getVariable', { key: 'public-share-data' }).then((data) => {
+		// Leave dataSnapshot part unchanged
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		const { dataSnapshot, shareUrl } = data as any || {}
+		if (dataSnapshot) {
+			cy.restoreState(dataSnapshot)
+			context.url = shareUrl
+			return cy.wrap(shareUrl as string)
+		} else {
+			const shareData: Record<string, unknown> = {}
+			return cy.createRandomUser()
+				.then((user) => {
+					context.user = user
+				})
+				.then(() => setupData(context, shareName))
+				.then(() => createShare(context, shareName))
+				.then((url) => {
+					shareData.shareUrl = url
+				})
+				.then(() => adjustSharePermission())
+				.then(() =>
+					cy.saveState().then((snapshot) => {
+						shareData.dataSnapshot = snapshot
+					})
+				)
+				.then(() => cy.task('setVariable', { key: 'public-share-data', value: shareData }))
+				.then(() => cy.log(`Public share setup, URL: ${shareData.shareUrl}`))
+				.then(() => cy.wrap(context.url))
+		}
+	})
 }
